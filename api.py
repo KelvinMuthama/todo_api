d01d037bc1699fd05bb9bfb12f6f75b38cab1e98
Kelvin Muthama <kelvinkamau387@gmail.com>
2019-08-31 14:48:44 +0300

    Added a route for getting one user from the db

--- api.py
+++ api.py
@@ -102,6 +102,29 @@ def get_all_users(current_user):
 
     return jsonify({'users' : output})
 
+@app.route('/user/<public_id>', methods=['GET'])
+@token_required
+def get_one_user(current_user, public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+    
+    if not current_user.admin:
+        return jsonify({'message': 'Access Denied!'})
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+
+    return jsonify({'user': user_data})
+
+    
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
