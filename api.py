958e3ee918abb95774c5fc673471f1c1a69f7de0
Kelvin Muthama <kelvinkamau387@gmail.com>
2019-08-31 11:08:50 +0300

    Added a Todo model to the app

--- api.py
+++ api.py
@@ -14,12 +14,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///api.db'
 
 db = SQLAlchemy(app)
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(50))
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(50))
+    complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer)
 
 if __name__ == '__main__':
     app.run(debug=True)
