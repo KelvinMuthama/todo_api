bc891c7561483fa29cd42b02b2789151b1b44819
Kelvin Muthama <kelvinkamau387@gmail.com>
2019-09-02 21:25:26 +0300

    Added the todo endpoints to the task module

---
+++ api/task/routes.py
@@ -0,0 +1,80 @@
+from flask import request, jsonify, Blueprint
+from api import db
+from api.models import Todo
+from api.utils import token_required
+
+task = Blueprint('task', __name__)
+
+
+@task.route('/todo', methods=['POST'])
+@token_required
+def create_todo(current_user):
+    data = request.get_json()
+
+    new_todo = Todo(text=data['text'], complete=False, user_id=current_user.id)
+
+    db.session.add(new_todo)
+    db.session.commit()
+
+    return jsonify({'message': 'Todo created!'})
+
+
+@task.route('/todo', methods=['GET'])
+@token_required
+def get_all_todos(current_user):
+    todos = Todo.query.filter_by(user_id=current_user.id).all()
+
+    output = []
+
+    for todo in todos:
+        todo_data = {}
+        todo_data['id'] = todo.id
+        todo_data['text'] = todo.text
+        todo_data['comlete'] = todo.complete
+        output.append(todo_data)
+
+    return jsonify({'todos': output})
+
+
+@task.route('/todo/<todo_id>', methods=['GET'])
+@token_required
+def get_one_todo(current_user, todo_id):
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message': 'No todo found!'})
+
+    todo_data = {}
+    todo_data['id'] = todo.id
+    todo_data['text'] = todo.text
+    todo_data['complete'] = todo.complete
+
+    return jsonify(todo_data)
+
+
+@task.route('/todo/<todo_id>', methods=['PUT'])
+@token_required
+def complete_todo(current_user, todo_id):
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({"message": "No to do found!"})
+
+    todo.complete = True
+    db.session.commit()
+
+    return jsonify({"message": "Todo item has been completed!"})
+
+
+@task.route('/todo/<todo_id>', methods=['DELETE'])
+@token_required
+def delete_to(current_user, todo_id):
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({"message": 'No todo found!'})
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return jsonify({'message': 'Todo item deleted!'})
